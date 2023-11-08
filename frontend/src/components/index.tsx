de0f6fe9267d234908a6eaf867877852326b1189
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-11-08 20:26:53 +0800

    style: add particles background

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -4,7 +4,15 @@ import RegistrationForm from "./auth/RegistrationForm";
 import InputGroup from "./elements/InputGroup";
 
 import HorizontalRule from "./extras/HorizontalRule";
+import ParticlesBackground from "./extras/ParticlesBackground";
 
 import Banner from "./landing/Banner";
 
-export { LoginForm, RegistrationForm, InputGroup, HorizontalRule, Banner };
+export {
+    LoginForm,
+    RegistrationForm,
+    InputGroup,
+    HorizontalRule,
+    ParticlesBackground,
+    Banner,
+};
--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { ParticlesBackground } from "../components";
 
 const Root: React.FC<{}> = () => {
     return (
-        <>
+        <ParticlesBackground>
             <main>
                 <Outlet />
             </main>
-        </>
+        </ParticlesBackground>
     );
 };
 
