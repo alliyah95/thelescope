d73db09d47d0508970da848fb26a410a61c3087f
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-11-10 21:12:51 +0800

    fix: use new abi during contract deployment

--- ethereum/src/web3.js
+++ ethereum/src/web3.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const { Web3 } = require("web3");
 
 let web3;
-console.log(process.env.INFURA_API);
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
     window.ethereum.request({ method: "eth_requestAccounts" });
     web3 = new Web3(window.ethereum);
